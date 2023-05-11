42517617bde494287a1b60e034edfb5413cdda6e
Vinicius Hartmann Ferreira <vinihf@gmail.com>
2023-05-11 10:07:48 -0300

    alteração

--- quizz/script.js
+++ quizz/script.js
@@ -1,10 +1,20 @@
-const botao = document.querySelector('.botao');
+formulario = document.querySelector('form');
+formulario.addEventListener('submit',function(){
+    var questao1 = document.querySelector('input[name=questao1]:checked');
+    var questao2 = document.querySelector('input[name=questao2]:checked');
+    var pontos = 0;
+    if(questao1.value == 'c'){
+        pontos++;
+    }
+    if(questao2.value == 'a'){
+        pontos++;
+    }
 
-botao.addEventListener("click",function(e){
-    e.preventDefault();
-    const nome = document.querySelector('.nome').value;
-    const idade = document.querySelector('.idade').value;
-    const resultado = document.querySelector('.resultado');
-    resultado.style.display = 'block';
-    resultado.innerHTML = `Olá, ${nome}! Você tem ${idade} anos.`;
+    if(pontos==0){
+        alert(`Você acertou ${pontos} de 2 questões! Precisa estudar mais.`);
+    }else if (pontos==1){
+        alert(`Você acertou ${pontos} de 2 questões! Foi bem, mas pode melhorar.`);
+    }else{
+        alert(`Você acertou ${pontos} de 2 questões! Muito bem!`);
+    }
 });
